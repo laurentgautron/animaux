dc3c47a940c8fb5b34935367b5777e3f6f97a1e0
laurent GAUTRON <laurentgautron@protonmail.com>
2022-01-02 14:14:20 +0100

    [header]: botstrap colors with sass

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
 import 'bootswatch/dist/superhero/bootstrap.css'
+import './styles/app.scss';
+
 
 
 // start the Stimulus application

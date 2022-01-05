993ea4ecabe160461c3c34c6b4ae6240cbe0ca67
laurent GAUTRON <laurentgautron@protonmail.com>
2022-01-05 19:02:58 +0100

    remove mobile detect

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
-use Mobile_Detect;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,15 +12,9 @@ class UserController extends AbstractController
     #[Route('/user', name: 'app_user')]
     public function index( UserRepository $userRepository ): Response
     {
-        $detect = new Mobile_Detect();
-        $mobile = false;
-        if ($detect->is_mobile()) {
-            $mobile = true;
-        }
         $userList = $userRepository->findAll();
         return $this->render('user/index.html.twig',[
             'users' => $userList,
-            'mobile' => $mobile,
         ]);
     }
 }

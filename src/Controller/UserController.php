4f31f1a910c28bf4f5e5af3777f837ca84089820
laurent GAUTRON <laurentgautron@protonmail.com>
2022-01-06 18:12:07 +0100

    start user edit page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index( UserRepository $userRepository ): Response
+    public function index(UserRepository $userRepository ): Response
     {
         $userList = $userRepository->findAll();
         return $this->render('user/index.html.twig',[
             'users' => $userList,
+        ]);
+    }
+
+    #[Route('/user/read/{id}', name: 'app_user_read')]
+    public function edit(Request $request, User $user, UserRepository $userRepository, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        return $this->render('user/read.hml.twig', [
+            'form'  => $form->createView(),
         ]);
     }
 }

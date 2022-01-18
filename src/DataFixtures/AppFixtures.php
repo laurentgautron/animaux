889b8acf4c2992a5ad2fc9c2e2dc9cfa5f75728f
laurent GAUTRON <laurentgautron@protonmail.com>
2022-01-18 17:37:43 +0100

    make fake users improve

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {   
+        $this->hasher = $hasher;
+        $this->mailAdd = ["hotmail.com", "sfr.fr", "gmail.com", "free.fr", "orange.com"];
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $generator = Factory::create("fr_FR");
 
+        for ($i = 0; $i<40; $i++) {
+            $user = new User();
+            $user->setFirstName($generator->firstName);
+            $firstName = strtolower($user->getFirstName());
+            $user->setLastName($generator->lastName);
+            $lastName = strtolower($user->getLastName());
+            $email = $this->mailAdd[array_rand($this->mailAdd)];
+            $user->setEmail($lastName.$firstName.'@'.$email);
+            //$user->setEmail($generator->email);
+            $user->setRoles(["ROLE_COLL"]);
+            $user->setPassword($this->hasher->hashPassword($user, 'secret'));
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }

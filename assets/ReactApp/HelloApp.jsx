eada89b4b387e3f2e3648341b2dd5d6e973c4b87
laurent GAUTRON <laurentgautron@protonmail.com>
2022-01-21 15:07:57 +0100

    before react

--- assets/ReactApp/HelloApp.jsx
+++ assets/ReactApp/HelloApp.jsx
@@ -1,16 +1,2 @@
 import React from "react";
 
-class HelloApp extends React.Component {
-    render() {
-        return (
-           <div className="form-group my-5">
-               <form className="animalResearch d-flex">
-                   <input type="text" name="animalName" className="mx-3 p-1" placeholder="Ex: Lion"/>
-                   <button type="submit" className="btn btn-info">Rechercher</button>
-               </form>
-           </div>
-        )
-    }
-}
-
-export default HelloApp
